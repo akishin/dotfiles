1c0df71f9c9921113cfa892f75f35f136278f0c7
akishin <akishin999@gmail.com>
2014-09-07 09:14:29 +0900

    add .bundle/config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .zshrc .gitconfig .inputrc .gemrc .irbrc .pryrc .screenrc .vimrc .gvimrc .vimshrc .tmux.conf .vim/ftplugin/ruby.vim .percol.d)
+DOT_FILES=( .zshrc .gitconfig .inputrc .gemrc .irbrc .pryrc .screenrc .vimrc .gvimrc .vimshrc .tmux.conf .vim/ftplugin/ruby.vim .percol.d .bundle)
 
 # .vim ディレクトリが存在しなければ作成
 if [ ! -d $HOME/.vim/ftplugin ] ; then
